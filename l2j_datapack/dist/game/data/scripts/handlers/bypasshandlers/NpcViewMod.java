027cbb51848de0e2dcbfee0d21beb22b15c1cf58
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-07-21 02:48:31 +0300

    Aggro Improvements

--- l2j_datapack/dist/game/data/scripts/handlers/bypasshandlers/NpcViewMod.java
+++ l2j_datapack/dist/game/data/scripts/handlers/bypasshandlers/NpcViewMod.java
@@ -159,10 +159,10 @@ public class NpcViewMod implements IBypassHandler
 		stringBuilder.append("<body>");
 		stringBuilder.append("<center>");
 
-		stringBuilder.append("<table width=360 cellpadding=2 cellspacing=2 background=\"L2UI_CT1.Windows.Windows_DF_TooltipBG\">");
+		stringBuilder.append("<table width=320 cellpadding=2 cellspacing=2 background=\"L2UI_CT1.Windows.Windows_DF_TooltipBG\">");
 		stringBuilder.append("<tr>");
 		stringBuilder.append("<td><font color=\"LEVEL\"><b>Name</b></font></td>");
-		stringBuilder.append("<td><font color=\"LEVEL\"><b>Aggro</b></font></td>");
+		stringBuilder.append("<td><font color=\"LEVEL\"><b>Aggression</b></font></td>");
 		stringBuilder.append("</tr>");
 
 		npc.getAggroList().forEach((character, aggro) -> {
