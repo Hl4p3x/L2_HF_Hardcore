5d5591ac2ca7ba2517e9c6a3fb1168b53623da10
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-07-31 23:36:41 +0300

    Fix IdleBoss Manager

--- l2j_datapack/dist/game/data/scripts/ai/IdleBossManager.java
+++ l2j_datapack/dist/game/data/scripts/ai/IdleBossManager.java
@@ -12,6 +12,8 @@ public class IdleBossManager {
     private long checkDelay = Config.CHECK_ACTIVITY_DELAY;
     private long inactivityThreshold;
 
+    private static final long MINUTE_IN_MS = 60000;
+
     private String checkActivityTimerName = "CHECK_ACTIVITY";
 
     private Runnable onIdle;
@@ -20,23 +22,15 @@ public class IdleBossManager {
 
     public IdleBossManager(AbstractNpcAI npcAI, long inactivityThreshold, Runnable onIdle) {
         this.npcAI = npcAI;
-        this.inactivityThreshold = inactivityThreshold;
+        this.inactivityThreshold = inactivityThreshold * MINUTE_IN_MS;
         this.onIdle = onIdle;
-    }
-
-    public IdleBossManager(AbstractNpcAI npcAI, long inactivityThreshold, Runnable onIdle,
-        L2Npc controllerNpc) {
-        this.npcAI = npcAI;
-        this.inactivityThreshold = inactivityThreshold;
-        this.onIdle = onIdle;
-        this.controllerNpc = controllerNpc;
     }
 
     public IdleBossManager(AbstractNpcAI npcAI, long checkDelay, long inactivityThreshold,
         String checkActivityTimerName, Runnable onIdle, L2Npc controllerNpc) {
         this.npcAI = npcAI;
         this.checkDelay = checkDelay;
-        this.inactivityThreshold = inactivityThreshold;
+        this.inactivityThreshold = inactivityThreshold * MINUTE_IN_MS;
         this.checkActivityTimerName = checkActivityTimerName;
         this.onIdle = onIdle;
         this.controllerNpc = controllerNpc;
