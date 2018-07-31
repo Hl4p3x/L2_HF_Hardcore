1740bbaabbad0d4d62ecf415dc3d385ee6a3f2ee
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-08-01 02:04:18 +0300

    FlyTo Server-Client Coordinates Sync

--- l2j_server/src/main/java/com/l2jserver/gameserver/model/actor/tasks/character/FlyToLocationTask.java
+++ l2j_server/src/main/java/com/l2jserver/gameserver/model/actor/tasks/character/FlyToLocationTask.java
@@ -22,6 +22,7 @@ import com.l2jserver.gameserver.model.L2Object;
 import com.l2jserver.gameserver.model.actor.L2Character;
 import com.l2jserver.gameserver.model.skills.Skill;
 import com.l2jserver.gameserver.network.serverpackets.FlyToLocation;
+import com.l2jserver.gameserver.network.serverpackets.MoveToLocation;
 
 /**
  * Task dedicated to fly a player to the location
@@ -47,6 +48,7 @@ public final class FlyToLocationTask implements Runnable
 		{
 			_character.broadcastPacket(new FlyToLocation(_character, _target, _skill.getFlyType()));
 			_character.setXYZ(_target.getX(), _target.getY(), _target.getZ());
+            _character.broadcastPacket(new MoveToLocation(_character));
 		}
 	}
 }
