a50ca6c707bba93c1637fbd6ea39eb7b2a873c9f
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-09-03 21:00:26 +0300

    Set Solina Respawn Time

--- l2j_datapack/dist/sql/game/custom/set_solina_disciple_respawn.sql
+++ l2j_datapack/dist/sql/game/custom/set_solina_disciple_respawn.sql
@@ -1,1 +1,1 @@
-UPDATE spawnlist SET respawn_delay=100, location='monastery_of_silence' WHERE npc_templateid=22792
+UPDATE spawnlist SET respawn_delay=80, location='monastery_of_silence' WHERE npc_templateid=22792
