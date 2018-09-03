b191f96e3e361c0a199e2767e4e316596e67ff7b
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-09-03 21:00:04 +0300

    Random Enchantment limit to Weapons and Armor

--- l2j_server/src/main/java/com/l2jserver/gameserver/model/actor/RandomEnchantmentHelper.java
+++ l2j_server/src/main/java/com/l2jserver/gameserver/model/actor/RandomEnchantmentHelper.java
@@ -7,7 +7,7 @@ import com.l2jserver.util.Rnd;
 public class RandomEnchantmentHelper {
 
     public static void applyRandomDropEnchant(L2ItemInstance item) {
-        if (Config.ALT_DROP_ENCHANTED && Config.ALT_DROP_ENCHANTED_CHANCE > Rnd.get(100)) {
+        if (Config.ALT_DROP_ENCHANTED && (item.isWeapon() || item.isArmor()) && Config.ALT_DROP_ENCHANTED_CHANCE > Rnd.get(100)) {
             item.setEnchantLevel(Rnd.get(Config.ALT_DROP_ENCHANTED_MIN, Config.ALT_DROP_ENCHANTED_MAX));
         }
     }
