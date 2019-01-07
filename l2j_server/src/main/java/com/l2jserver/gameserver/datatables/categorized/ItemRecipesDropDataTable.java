79dc127a4994c94aca5bb6543ba49029f61b6380
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2019-01-08 01:20:08 +0200

    Remove Recipeless Items from lookup

--- l2j_server/src/main/java/com/l2jserver/gameserver/datatables/categorized/ItemRecipesDropDataTable.java
+++ l2j_server/src/main/java/com/l2jserver/gameserver/datatables/categorized/ItemRecipesDropDataTable.java
@@ -43,6 +43,86 @@ public class ItemRecipesDropDataTable implements EquipmentProvider<L2RecipeList>
         LOG.info("Loaded {} item recipes for dynamic loot", recipeIds.size());
     }
 
+    List<Integer> RECIPELESS_ITEMS = Arrays.asList(
+            185, // Staff of Mana
+            293, // War Hammer
+            143, // Sword of Mystic
+            296, // Dwarven Pike
+            292, // Pike
+            316, // Sage's Blood
+            130, // Elven Sword
+            86, // Tomahawk
+            240, // Conjurer's Knife
+            222, // Poniard Dagger
+            179, // Mace of Prayer
+            83, // Sword of Magic
+            259, // Single-Edged Jamadhr
+            144, // Sword of Occult
+            186, // Staff of Magic
+            127, // Crimson Sword
+            223, // Kukuri
+            238, // Dagger of Mana
+            239, // Mystic Knife
+            260, // Triple-Edged Jamadhr
+            312, // Branch of Life
+            178, // Bone Staff
+            69, // Bastard Sword
+            182, // Doom Hammer
+            101, // Scroll of Wisdom
+            258, // Bagh-Nakh
+            275, // Long Bow
+            220, // Crafted Dagger
+            168, // Work Hammer
+            183, // Mystic Staff
+            126, // Artisan's Sword
+            274, // Reinforced Bow
+            166, // Heavy Mace
+            156, // Hand Axe
+            315, // Divine Tome
+            314, // Proof of Revenge
+            277, // Dark Elven Bow
+            436, // Tunic of Knowledge
+            469, // Stockings of Knowledge
+            378, // Compound Scale Gaiters
+            351, // Blast Plate
+            416, // Reinforced Leather Gaiters
+            349, // Compound Scale Mail
+            394, // Reinforced Leather Shirt
+            59, // Mithril Gaiters
+            2446, // Reinforced Leather Gloves
+            58, // Mithril Breastplate
+            379, // Dwarven Scale Gaiters
+            350, // Dwarven Scale Mail
+            47, // Helmet
+            2422, // Reinforced Leather Boots
+            414, // Lion Skin Gaiters
+            627, // Aspis
+            2447, // Gloves of Knowledge
+            465, // Cursed Stockings
+            376, // Iron Plate Gaiters
+            2423, // Boots of Knowledge
+            605, // Leather Gauntlets
+            1123, // Blue Buckskin Boots
+            435, // Mystic's Tunic
+            46, // Bronze Helmet
+            391, // Puma Skin Shirt
+            347, // Ring Mail Breastplate
+            628, // Hoplon
+            392, // Lion Skin Shirt
+            468, // Mystic's Stockings
+            413, // Puma Skin Gaiters
+            432, // Cursed Tunic
+            626, // Bronze Shield
+            63, // Gauntlets
+            912, // Near Forest Necklace
+            880, // Black Pearl Ring
+            847, // Red Crescent Earring
+            890, // Ring of Devotion
+            879, // Enchanted Ring
+            848 // Enchanted Earring
+    );
+
+
     private Map<GradeInfo, List<L2RecipeList>> buildRecipeData(Map<GradeInfo, List<GradedItem>> getGradedWeaponsMap) {
         Map<GradeInfo, List<L2RecipeList>> recipesByGrade = new HashMap<>();
         getGradedWeaponsMap.forEach((gradeInfo, gradedItems) -> {
@@ -50,7 +130,7 @@ public class ItemRecipesDropDataTable implements EquipmentProvider<L2RecipeList>
                     .stream()
                     .map(gradedItem -> {
                         Optional<L2RecipeList> recipeOption = RecipeData.getInstance().getRecipeByProductionItem(gradedItem.getItemId());
-                        if (!recipeOption.isPresent() && !gradedItem.getGradeInfo().getGrade().equals(Grade.NG)) {
+                        if (recipeOption.isEmpty() && (!gradedItem.getGradeInfo().getGrade().equals(Grade.NG) || !RECIPELESS_ITEMS.contains(gradedItem.getItemId()))) {
                             LOG.warn("Could not find recipe for item {}", gradedItem);
                         }
                         return recipeOption;
